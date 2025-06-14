7250620dbe755b0117c688257d0d7a6da3cd8153
EddyGiusepe <eddychirinos.unac@gmail.com>
2025-06-14 14:43:07 -0300

    A nossa função Lambda usada na AWS

--- agent_sales_on_AWS/dummy_lambda.py
+++ agent_sales_on_AWS/dummy_lambda.py
@@ -2,32 +2,38 @@
 """
 Senior Data Scientist.: Dr. Eddy Giusepe Chirinos Isidro
 """
-
 def lambda_handler(event, context):
-        agent = event['agent']
-        actionGroup = event['actionGroup']
-        function = event['function']
-        parameters = event.get('parameters', [])
+    agent = event['agent']
+    actionGroup = event['actionGroup']
+    function = event['function']
+    parameters = event.get('parameters', [])
 
-        def process_return(orderNumber):
-            # Call the return API
-            message = "The item was returned successfully."
-            print(message)
+    def process_return(orderNumber):
+        # Call the return API
+        message = "The item was returned successfully."
+        print(message)
 
-        responseBody = {}
-        if function == "process_return":
-            # Extract the orderNumber from the parameters
-            orderN = "abc123"
-            process_return(orderN)
-            responseBody = {
-                "TEXT": {
-                    "body": "Process Return function was called successfully."
+    responseBody = {}
+    if function == "process_return":
+        # Extract the orderNumber from the parameters
+        orderN = "abc123"
+        process_return(orderN)
+        body_content = "Process Return function was called successfully."
+    else:
+        body_content = "The order function {} was called successfully!".format(function)
+    
+    # Formato correto da resposta para o Bedrock Agent
+    return {
+        "messageVersion": "1.0",
+        "response": {
+            "actionGroup": actionGroup,
+            "function": function,
+            "functionResponse": {
+                "responseBody": {
+                    "TEXT": {
+                        "body": body_content
+                    }
                 }
             }
-        
-        else:
-            responseBody = {
-                "TEXT": {
-                    "body": "The order function {} was called successfully!".format(function)
-                }
-            }
+        }
+    }
